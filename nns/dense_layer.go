803a606a51556e993b220f76afb38160c44cf451
Anand KC <54168288+anandkc812@users.noreply.github.com>
2020-05-30 11:53:29 +0530

    Delta function added

--- nns/dense_layer.go
+++ nns/dense_layer.go
@@ -134,6 +134,38 @@ func fillRand( x *mat64.Dense) {
 
 }
 
+func Backprop( input *mat64.Dense, dlayer denseLayer) {
+
+
+}
+
+// Calculate Delta of the last /output layer
+func OutputDeltaCalc( groundtruth_values *mat64.Dense, curr_layer denseLayer) {
+
+	
+	r1, c1 := curr_layer.output.Dims()
+	r2, c2 := groundtruth_values.Dims()
+
+	fmt.Println("Output dims", r1,c1)
+	fmt.Println("groundtruth_values dims", r2,c2)
+	
+
+	curr_layer.deltas.Sub(curr_layer.output, groundtruth_values.T())
+	
+	r3, c3 := curr_layer.deltas.Dims()
+	fmt.Println("deltas dims", r3,c3)
+	r4, c4 := curr_layer.derivatives.Dims()
+	fmt.Println("derivatives dims", r4,c4)
+	
+	
+	
+	
+	
+	curr_layer.deltas.MulElem(curr_layer.deltas, curr_layer.derivatives) 
+
+}
+
+
 func NewDense() {
 
 	makeDense := [3]int{10,12,3}
@@ -164,8 +196,14 @@ func NewDense() {
 func test_1() {
 
 	input := mat64.NewDense(10, 1, nil)
-	output := &mat64.Dense{}
+	output := mat64.NewDense(3, 1, nil)
 	
+	fmt.Println("Expected output set begin");
+	output.Set(0, 0, 0.9)
+	output.Set(1, 0, 0.0)
+	output.Set(2, 0, 0.9)
+	
+	fmt.Println("Expected output set");
 	
 	fillRand(input)
 	makeDense := [3]int{10,12,3}
@@ -180,6 +218,9 @@ func test_1() {
 	
 	Forward(dLHidden.input,dLHidden)  
 
+
+	OutputDeltaCalc(output, dLHidden)
+	
 	fmt.Println(output)
 	
 
