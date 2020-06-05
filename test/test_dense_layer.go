3eb838c9d4bb04ac5c5ce623da7a6b8d25d155c3
Anand KC <54168288+anandkc812@users.noreply.github.com>
2020-06-05 14:41:23 +0530

    Fix test cases

--- test/test_dense_layer.go
+++ test/test_dense_layer.go
@@ -46,13 +46,71 @@ func dense_test1()  {
 }
 
 
+
+
+func test_1() {
+
+	input := mat64.NewDense(10, 1, nil)
+	output := mat64.NewDense(3, 1, nil)
+	
+	fmt.Println("Expected output set begin");
+	output.Set(0, 0, 0.9)
+	output.Set(1, 0, 0.0)
+	output.Set(2, 0, 0.9)
+	
+	fmt.Println("Expected output set");
+	
+	for i:=0; i< 10;i++ {
+		input.Set(i, 0, 0.1*float64(i))
+	}
+	
+	//nns.fillRand(input)
+	makeDense := [3]int{10,12,3}
+	
+	fmt.Println("------------TEST 1 ----------")
+	dL_InputLayer := nns.NewDenseLayer( "Input Layer ", makeDense[0], makeDense[1], nil, nil)
+	dLHidden      := nns.NewDenseLayer( "Hidden Layer ", makeDense[1], makeDense[2], nil, nil)
+	
+
+	//dL_InputLayer.input = input
+	
+	_, _ = nns.SetInput(input, &dL_InputLayer)
+	
+	nns.Forward(input,dL_InputLayer)  
+	
+	//dLHidden.input = dL_InputLayer.output
+	nns.LinkLayers(&dL_InputLayer,&dLHidden)
+	
+	yout_mat64 := nns.GetOutput(dL_InputLayer)
+	nns.Forward(yout_mat64,dLHidden)  
+
+
+	nns.OutputDeltaCalc(output, dLHidden)
+	nns.Update(dLHidden, 0.5)
+
+	nns.Backprop( dL_InputLayer, dLHidden)
+
+	yout_mat64 = nns.GetOutput(dLHidden)
+	mse_err := nns.Mse(output, yout_mat64)
+	
+	nns.Update(dL_InputLayer, 0.5)
+	
+	fmt.Println("Mse error ", mse_err)
+	
+	fmt.Println(output)
+	
+
+}
+
+
 func main() {
 
 
-	dense_test1()
-	nns.NewDense()
+	//dense_test1()
+	test_1()
+	//nns.NewDense()
 	
-	fmt.Println("Dense Test 1 completed")
+	//fmt.Println("Dense Test 1 completed")
 	
 	
 
